093b5774c9ca7658f7b28f8729fea3d0b296486f
Andrey Carvajal <ancarsa.wasabi.12@gmail.com>
2023-09-05 16:40:08 -0500

    WASABITOdormilon

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,6 +1,4 @@
 import React, {Component} from 'react';
-import {connect} from "react-redux";
-import {goPage, nextPage, prevPage} from "../../actions";
 
 class Pagination extends Component {
 
@@ -119,11 +117,5 @@ class Pagination extends Component {
 }
 
 
-const mapStateToProps = (state) => {
-    return {
-        ...state.pagination,
-        totalItemsCount: state.shop.products.length,
-    }
-};
 
 export default Pagination;
