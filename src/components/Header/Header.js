92d24d676ed71238127f0b750dc1222abc96c523
Andrey Carvajal <ancarsa.wasabi.12@gmail.com>
2023-09-10 14:05:56 -0500

    logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,49 +1,46 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {NavLink} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
-const Header = ({cartLength}) => {
-
-    return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-            <div className="container">
-                <NavLink className="navbar-brand" to="/">My Online Store</NavLink>
-                <div>
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to={"/cart"}><i className="fa fa-shopping-cart mr-2"
-                                                                          aria-hidden="true" />Cart {cartLength ? `(${cartLength})`: ''}</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    );
+const Header = ({ cartLength }) => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+      <div className="container">
+      <NavLink className="navbar-brand" to="/">
+    <img
+      src="https://raw.githubusercontent.com/Wasabi95/NavBar-SideMenu/master/images/cc.png"
+      alt="Logo"
+      className="logo-image"
+    />
+    My Online Store
+  </NavLink>
+        <div>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/cart">
+                <i className="fa fa-shopping-cart mr-2" aria-hidden="true" /> Cart{' '}
+                {cartLength ? `(${cartLength})` : ''}
+              </NavLink>
+            </li>
+            {/* Add a Login NavLink */}
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/login">
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
-
 
 const mapStateToProps = (state) => {
   return {
-      cartLength: state.shop.cart.length
-  }
+    cartLength: state.shop.cart.length,
+  };
 };
 
 export default connect(mapStateToProps, null)(Header);
 
 
-/*
-*                         <li className="nav-item active">
-                            <a className="nav-link" href="#">Home
-                                <span className="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Services</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
-                        </li>
-* */
